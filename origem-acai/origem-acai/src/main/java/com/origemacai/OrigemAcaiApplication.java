ec3886c5c6f02bf57300dd03f8da8f1dc94f4b33
Victor <victoremmadrid@outlook.com>
2025-03-30 18:25:53 +0200

    hazaña: estructura inicial con Spring Boot y paquetes base

---
+++ origem-acai/origem-acai/src/main/java/com/origemacai/OrigemAcaiApplication.java
@@ -0,0 +1,13 @@
+package com.origemacai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrigemAcaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrigemAcaiApplication.class, args);
+	}
+
+}
---
+++ origem-acai/origem-acai/src/test/java/com/origemacai/OrigemAcaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origemacai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrigemAcaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
